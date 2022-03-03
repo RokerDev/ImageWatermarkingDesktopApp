44a9c44ab84923b6c564f6f7376e5aeca4f5e900
RokerDev <rokerdev91@gmail.com>
2022-03-04 00:13:28 +0100

    clean some code

--- main.py
+++ main.py
@@ -44,12 +44,20 @@ text_height = 0
 def read_image():
     global image, image_size
     img_path = filedialog.askopenfilename()
+
     with Image.open(img_path) as picture:
-        image = picture
-        image_size = image.size
+
         tk_image = ImageTk.PhotoImage(picture)
         lbl_image.config(image=tk_image)
         lbl_image.image = tk_image
+        image = picture
+        image_size = image.size
+
+
+def convert_image_to_png(picture_obj, path):
+    if not path.endswith(".png"):
+        path, extension = path.split(".")
+        picture_obj.save(f"{path}.png")
 
 
 def set_watermark_font_size(*args):
@@ -100,7 +108,8 @@ def display_image():
     g = int(g)
     b = int(b)
 
-    d.text((text_x_pos, text_y_pos), mark_text, font=text_font, fill=(r, g, b, text_opacity))
+    d.text((text_x_pos, text_y_pos-50), mark_text,
+           font=text_font, fill=(r, g, b, text_opacity))
 
     out = Image.alpha_composite(image, txt)
     out = ImageTk.PhotoImage(out)
@@ -110,53 +119,63 @@ def display_image():
     # out.show()
 
 
+# set window
 window = tk.Tk()
 window.title("Image Watermarking Desktop Application")
 window.columnconfigure(0, minsize=500, weight=50)
 window.columnconfigure(1, minsize=100, weight=1)
 window.rowconfigure(0, minsize=500, weight=1)
+
+# image frame
 frm_image = tk.Frame(window)
 frm_image.grid(row=0, column=0, sticky="nsew", )
-lbl_image = tk.Label(frm_image)
-lbl_image.pack()
 
+# place for image
+lbl_image = tk.Label(frm_image, relief=tk.SUNKEN, text="Read Image")
+lbl_image.grid(row=0, column=0, sticky="nsew")
+
+# options frame
 frm_options = tk.Frame(window, borderwidth=3)
 frm_options.grid(row=0, column=1, sticky="N", padx=3, pady=3)
+
+# detekt changes in ent_set_text - place to set watermark text
 watermark_text = tk.StringVar(master=frm_options)
 watermark_text.trace("w", set_watermark_text)
-btn_read_image = tk.Button(frm_options, text="Read Image", command=read_image)
+
+# read image button
+btn_read_image = tk.Button(frm_options, text="Read Image",
+                           command=read_image, )
 btn_read_image.grid(row=1, column=0, sticky="ew", padx=2, pady=2)
 
-ent_set_text = tk.Entry(frm_options, text="Set Text", textvariable=watermark_text)
+# read watermark text
+ent_set_text = tk.Entry(frm_options, text="Set Text",
+                        textvariable=watermark_text, )
 ent_set_text.grid(row=2, column=0, sticky="ew", padx=2, pady=2, )
 
-btn_change_color = tk.Button(frm_options, text="Change Color", command=set_watermark_text_color)
+# change watermark text
+btn_change_color = tk.Button(frm_options, text="Change Color",
+                             command=set_watermark_text_color, )
 btn_change_color.grid(row=3, column=0, sticky="ew", padx=2, pady=2, )
 
-scl_size = tk.Scale(
-    master=frm_options,
-    from_=10,
-    to=max_text_size,
-    orient="horizontal",
-    label="Size",
-    command=set_watermark_font_size
-)
+# change size of watermark text
+scl_size = tk.Scale(master=frm_options, from_=10, to=max_text_size,
+                    orient="horizontal", label="Size",
+                    command=set_watermark_font_size, )
 scl_size.grid(row=4, column=0, sticky="ew", padx=2, pady=2, )
 
-scl_set_X = tk.Scale(master=frm_options,
-                     from_=0,
-                     to=1000,
-                     orient="horizontal",
-                     label="X pos",
-                     command=set_watermark_x_pos)
+# set top left corner watermark text position
+scl_set_X = tk.Scale(master=frm_options, from_=0, to=1000, orient="horizontal",
+                     label="X pos", command=set_watermark_x_pos)
 scl_set_X.grid(row=5, column=0, sticky="ew", padx=2, pady=2, )
 
-scl_set_Y = tk.Scale(master=frm_options, from_=0, to=1000, orient="horizontal", label="Y pos",
-                     command=set_watermark_y_pos)
+# set top left corner watermark text position
+scl_set_Y = tk.Scale(master=frm_options, from_=0, to=1000, orient="horizontal",
+                     label="Y pos", command=set_watermark_y_pos, )
 scl_set_Y.grid(row=6, column=0, sticky="ew", padx=2, pady=2, )
 
-scl_opacity = tk.Scale(master=frm_options, from_=0, to=255, label="Opacity", orient="horizontal",
-                       command=set_watermark_opacity)
+# set opacity of watermark text
+scl_opacity = tk.Scale(master=frm_options, from_=0, to=255, label="Opacity",
+                       orient="horizontal", command=set_watermark_opacity, )
 scl_opacity.grid(row=7, column=0, sticky="ew", padx=2, pady=2, )
 
 window.mainloop()
