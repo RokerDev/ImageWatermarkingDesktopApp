0fd2356bcd29a2eab4f2261cf2666142c806942c
RokerDev <rokerdev91@gmail.com>
2022-03-09 20:28:34 +0100

    refactor: change the way displaying of watermark image from label to canvas

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import random
 import tkinter as tk
 from tkinter import filedialog, colorchooser
+
 from PIL import Image, ImageTk, ImageFont, ImageDraw
 from matplotlib import font_manager
 
 image = None
+canvas_image = None
 # default size of an image
-image_size = (500, 500)
+image_size = (1000, 1000)
 
 watermark_text_dict = {}
 
@@ -39,18 +40,21 @@ max_text_size = 200
 text_width = 0
 text_height = 0
 
-text_center = None
+text_center = []
 
 
 def read_image():
-    global image, image_size, text_center, text_y_pos, text_x_pos, text_width, text_height
+    global image, image_size, text_center, text_y_pos, text_x_pos, text_width, text_height, canvas_image
+
     img_path = filedialog.askopenfilename()
     if img_path != "":
         with Image.open(img_path) as picture:
-            # tk_image = ImageTk.PhotoImage(picture)
+            image = picture.convert("RGBA")
+            tk_image = ImageTk.PhotoImage(image)
             # lbl_image.config(image=tk_image)
             # lbl_image.image = tk_image
-            image = picture.convert("RGBA")
+            cnv_image.config(width=image.size[0], height=image.size[1])
+            canvas_image = cnv_image.create_image(0, 0, anchor="nw", image=tk_image)
             image_size = image.size
             enable_buttons()
             text_width, text_height = ImageDraw.Draw(image).textsize(mark_text, text_font)
@@ -82,6 +86,7 @@ def enable_buttons():
     scl_set_Y.config(state=tk.NORMAL)
     scl_opacity.config(state=tk.NORMAL)
 
+
 # def convert_image_to_png(picture_obj, path):
 #     if not path.endswith(".png"):
 #         path, extension = path.split(".")
@@ -97,7 +102,7 @@ def set_watermark_font_size(*args):
     text_y_pos = text_center[1] - text_height / 2
     # fr = 0 - text_height
     from_height = 0 - image_size[1] / 2 - text_height / 2
-    to_height = image_size[1] / 2 + text_height/2
+    to_height = image_size[1] / 2 + text_height / 2
     scl_set_Y.config(from_=from_height, to=to_height)
     # scl_set_Y.set(text_center[1] - text_height / 2)
 
@@ -122,7 +127,7 @@ def set_watermark_x_pos(*args):
 def set_watermark_y_pos(*args):
     global text_y_pos, text_center
 
-    text_y_pos = int(args[0])+image_size[1]/2-text_height/2
+    text_y_pos = int(args[0]) + image_size[1] / 2 - text_height / 2
     text_center = (text_x_pos + text_width / 2, text_y_pos + text_height / 2)
     print(text_center)
     display_image()
@@ -162,8 +167,8 @@ def display_image():
 
     out = Image.alpha_composite(image, txt)
     out = ImageTk.PhotoImage(out)
-    lbl_image.config(image=out)
-    lbl_image.image = out
+    cnv_image.itemconfig(canvas_image, image=out)
+    cnv_image.image = out
 
     # out.show()
 
@@ -176,15 +181,15 @@ window.columnconfigure(1, minsize=100, weight=1)
 window.rowconfigure(0, minsize=500, weight=1)
 
 # image frame
-frm_image = tk.Frame(window)
+frm_image = tk.Frame(master=window)
 frm_image.grid(row=0, column=0, sticky="nsew", )
 
 # place for image
-lbl_image = tk.Label(frm_image, relief=tk.SUNKEN, text="Read Image")
-lbl_image.pack()
+cnv_image = tk.Canvas(master=frm_image, relief=tk.SUNKEN, )
+cnv_image.pack()
 
 # options frame
-frm_options = tk.Frame(window, borderwidth=3)
+frm_options = tk.Frame(master=window, borderwidth=3)
 frm_options.grid(row=0, column=1, sticky="N", padx=3, pady=3)
 
 # detekt changes in ent_set_text - place to set watermark text
@@ -215,17 +220,17 @@ scl_size.grid(row=4, column=0, sticky="ew", padx=2, pady=2, )
 
 # set top left corner watermark text position
 scl_set_X = tk.Scale(master=frm_options, from_=0, to=1000, orient="horizontal",
-                     label="X pos", command=set_watermark_x_pos, state=tk.DISABLED,)
+                     label="X pos", command=set_watermark_x_pos, state=tk.DISABLED, )
 scl_set_X.grid(row=5, column=0, sticky="ew", padx=2, pady=2, )
 
 # set top left corner watermark text position
 scl_set_Y = tk.Scale(master=frm_options, from_=0, to=1000, orient="horizontal",
-                     label="Y pos", command=set_watermark_y_pos, state=tk.DISABLED,)
+                     label="Y pos", command=set_watermark_y_pos, state=tk.DISABLED, )
 scl_set_Y.grid(row=6, column=0, sticky="ew", padx=2, pady=2, )
 
 # set opacity of watermark text
 scl_opacity = tk.Scale(master=frm_options, from_=0, to=255, label="Opacity",
                        orient="horizontal", command=set_watermark_opacity, state=tk.DISABLED, )
-scl_opacity.grid(row=7, column=0, sticky="ew", padx=2, pady=2,)
+scl_opacity.grid(row=7, column=0, sticky="ew", padx=2, pady=2, )
 
 window.mainloop()
